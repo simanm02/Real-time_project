1da62f154e87e339d7aa8d6b250d5c99e23b25cc
Audby <130966033+Audby@users.noreply.github.com>
2025-03-06 16:52:37 +0100

    More clean-up of main.rs

--- src/main.rs
+++ src/main.rs
@@ -52,10 +52,8 @@ fn serve_call (elevator: &mut Elevator, floor: u8) {
     if let Some(pos) = elevator.call_buttons.iter().position(|call|
         call[0] == floor && (call[1] == *serving_call || call[1] == e::CAB))  {
         
-        // Stop elevator at desired floor
         elevator.motor_direction(e::DIRN_STOP);
         
-        // Turn off call button light for served floor and call button
         elevator.call_button_light(floor,*serving_call, false);
         
         // Remove call from queue
@@ -114,47 +112,42 @@ fn main() -> std::io::Result<()> {
     // Polling period which reads sensor data at 25 ms intervals.
     let poll_period = Duration::from_millis(25);
 
-    // Creates a crossbream channel, so that the call buttons.
-    // can receive and transmitt messages via TCP.
+    // Four crossbeam channels to receive and transmitt TCP messages:
+
+    // Crossbeam for call buttons
     let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
     {
         // Clone the elevator handle so that there can be a new thread dedicated to it.
         let elevator = elevator.clone();
 
-        // "Spawn" a new dedicated thread that continuously polls
-        // call button presses and sends them (when pressed) to call_button_tx
+        // Spawns a new dedicated thread that continuously polls
         spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
     }
 
-    // Create crossbeam channel for the floor sensors when elevator passes.
+    // Crossbeam for floor sensor
     let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
     {
-        // Again clones and creates another dedicated thread to poll floor sensor data.
         let elevator_new = elevator.clone();
         spawn(move || elevio::poll::floor_sensor(elevator_new, floor_sensor_tx, poll_period));
     }
-
-    // Create crossbeam channel for stoppbuttons
+    
+    // Crossbeam for stop button
     let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
     {
-        // Clone and create dedicated thread to poll stop button data.
         let elevator = elevator.clone();
         spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
     }
 
-    // Create crossbeam channel for obstruction lever
+    // Crossbeam for obstruction lever
     let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
     {
-        // Clone and create dedicated thread to poll pull lever data
         let elevator = elevator.clone();
         spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
     }
-    // let (buttonarray_tx, buttonarray_rx) = cbc::unbounded::<Vec<u8>>();
 
-    // Define variable 'dirn' to keep track of current direction; down, up or stop.
     let dirn = e::DIRN_STOP;
 
-    //If the elevator isn't on a specific floor when we start, move down until it reaches one.
+    // If the elevator isn't on a specific floor when we start, move down until it reaches one.
     if elevator.floor_sensor().is_none() {
         elevator.motor_direction(e::DIRN_DOWN);
     }
@@ -233,6 +226,7 @@ fn main() -> std::io::Result<()> {
                     continue;
                 }
             },
+
             // If we receive that there is an obstruction:
             recv(obstruction_rx) -> obstruction => {
                 let mut obstr = obstruction.unwrap();
